b5a952f12215cbcab7d174ea91d3418041a01a98
Josh Bickett <josh@bickett.net>
2022-05-18 22:50:49 -0700

    Add api call

---
+++ src/apis/FlaskAPI.js
@@ -0,0 +1,19 @@
+export const getContent = async () => {
+  var formdata = new FormData();
+  formdata.append("name", "John");
+
+  var request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      body: formdata,
+      Accept: "application/json",
+    },
+  };
+
+  const result = await fetch("http://127.0.0.1:5000/get_content", request).then(
+    (response) => response.json()
+  );
+  return result;
+};
